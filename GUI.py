7b82e3c6029536a736783cc4e8a3da41a904df08
Kamil <kamil.kolodziejski44@gmail.com>
2021-11-13 20:45:58 +0100

    Bug fixed, better event handling

--- GUI.py
+++ GUI.py
@@ -1,40 +1,43 @@
-from tkinter import filedialog as fd
+from tkinter.filedialog import askopenfilename
 from tkinter.filedialog import asksaveasfilename
 from tkinter.messagebox import showinfo
 from tkinter.messagebox import showerror
 from FindAndDelete import *
 
 
-def ask_for_input(event):
-    global input
-    filetypes = (
-        ('PDF files', '*.pdf'),
-        ('All files', '*.*')
-    )
-    input = fd.askopenfilename(
-        title='Open a file',
-        initialdir='/',
-        filetypes=filetypes)
+class ButtonMethods:
+    """Class that wraps all methods bind with buttons"""
+    def __init__(self):
+        self.input = None
+        self.output = None
 
+    def ask_for_input(self, event):
+        filetypes = (
+            ('PDF files', '*.pdf'),
+            ('All files', '*.*')
+        )
+        self.input = askopenfilename(
+            title='Open a file',
+            initialdir='/',
+            filetypes=filetypes)
 
-def ask_for_output(event):
-    global output
-    files = [('PDF files', '*.pdf'),
-             ('All files', '*.*')]
-    output = asksaveasfilename(filetypes=files, defaultextension=files)
+    def ask_for_output(self, event):
+        files = [('PDF files', '*.pdf'),
+                 ('All files', '*.*')]
+        self.output = asksaveasfilename(filetypes=files, defaultextension=files)
 
-def run_app(event):
-    if input is None:
-        showerror("Błąd", "Nie wybrano pliku!")
-    else:
-        try:
-            delete_slides(input, output)
-            showinfo("Sukces!", f'Plik został zapisany w {output}')
-        except NameError:
-            delete_slides(input, set_defaults_paths())
-            showinfo("Sukces!", f'Plik został zapisany w {set_defaults_paths()}')
+    def run_app(self, event):
+        if self.output is None or self.output == '':
+            self.output = self.set_default_path()
+        if self.input is None:
+            showerror("Błąd", "Nie wybrano pliku!")
+        else:
+            try:
+                delete_slides(self.input, self.output)
+                showinfo("Sukces!", f'Plik został zapisany w {self.output}')
+            except Exception as e:
+                showerror("Błąd!", f'Tworzenie nowego pliku nie powiodło się: {e}')
 
-def set_defaults_paths():
-    # Default path
-    # return f'{os.getcwd()}\\pdf_short.pdf'
-    return f'{input[:-4]}_short.pdf'
+    def set_default_path(self):
+        # Default path
+        return f'{self.input[:-4]}_short.pdf'
--- main.py
+++ main.py
@@ -1,26 +1,34 @@
 import tkinter as tk
-from GUI import *
+from GUI import ButtonMethods
 
 
 if __name__ == '__main__':
     # Create window
     window = tk.Tk()
-    window.title('Tkinter Open File Dialog')
+    window.title('Usuwacz slajdów')
 
-    greeting = tk.Label(text="Wybierz plik pdf oraz miejsce zapisu.", width=50, height=10)
-    greeting.pack()
+    # Create class that will control source scripts
+    BT = ButtonMethods()
 
-    button_input = tk.Button(text="Select File", width=25, height=5)
-    button_input.bind("<Button-1>", ask_for_input)
+    instruction_text = "Wybierz plik pdf oraz miejsce zapisu.\n" + \
+                       "Domyślna ścieżka zapisu to ścieżka_pliku+'_short.pdf'"
+    instruction = tk.Label(text=instruction_text, width=50, height=5)
+    instruction.pack()
+
+    button_input = tk.Button(text="Wybierz plik", width=25, height=5)
+    button_input.bind("<Button-1>", lambda e: BT.ask_for_input(e))
     button_input.pack()
 
-    button_output = tk.Button(text="Save", width=25, height=5)
-    button_output.bind("<Button-1>", ask_for_output)
+    button_output = tk.Button(text="Wybierz miejsce zapisu", width=25, height=5)
+    button_output.bind("<Button-1>", lambda e: BT.ask_for_output(e))
     button_output.pack()
 
-    button_go = tk.Button(text='RunApp', width=25, height=5)
-    button_go.bind("<Button-1>", run_app)
+    button_go = tk.Button(text='GO!', width=25, height=5)
+    button_go.bind("<Button-1>", lambda e: BT.run_app(e))
     button_go.pack()
 
-    window.mainloop()
+    bottom_label = tk.Label(text="This app is inspired by Matthew BrotherInLaw", width=50, height=4)
+    bottom_label.config(font=("Garamond", 8))
+    bottom_label.pack()
 
+    window.mainloop()
